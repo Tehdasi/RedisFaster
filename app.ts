d6703f6eb0d781e31e6949ee39673cca5db45ca5
Daniel Papp <tehdasi@gmail.com>
2023-09-11 15:56:01 +1000

    1st commit

---
+++ app.ts
@@ -0,0 +1,77 @@
+import express from "express";
+import { createClient } from "redis";
+import { json } from "body-parser";
+
+const DEFAULT_BALANCE = 100;
+
+interface ChargeResult {
+    isAuthorized: boolean;
+    remainingBalance: number;
+    charges: number;
+}
+
+async function connect(): Promise<ReturnType<typeof createClient>> {
+    const url = `redis://${process.env.REDIS_HOST ?? "localhost"}:${process.env.REDIS_PORT ?? "6379"}`;
+    console.log(`Using redis URL ${url}`);
+    const client = createClient({ url });
+    await client.connect();
+    return client;
+}
+
+async function reset(account: string): Promise<void> {
+    const client = await connect();
+    try {
+        await client.set(`${account}/balance`, DEFAULT_BALANCE);
+    } finally {
+        await client.disconnect();
+    }
+}
+
+
+async function charge(account: string, charges: number): Promise<ChargeResult> {
+    const client = await connect();
+    try {
+        let rs= await client.eval( 
+            "local v= redis.call('GET','account/balance');\n" +
+            `local chr= ${charges};\n` +
+            "local nv= v- chr;\n" +
+            "if(nv>= 0 ) then\n" +
+            "   redis.call('SET','account/balance', nv );\n" +
+            "   return {1,nv, chr };\n" +
+            "else \n" +
+            "   return {0,v,0};\n" + 
+            "end") as number[];
+
+        return  { isAuthorized: rs[0] == 1, remainingBalance: rs[1], charges: rs[2] };
+    } finally {
+        await client.disconnect();
+    }
+}
+
+export function buildApp(): express.Application {
+    const app = express();
+    app.use(json());
+    app.post("/reset", async (req, res) => {
+        try {
+            const account = req.body.account ?? "account";
+            await reset(account);
+            console.log(`Successfully reset account ${account}`);
+            res.sendStatus(204);
+        } catch (e) {
+            console.error("Error while resetting account", e);
+            res.status(500).json({ error: String(e) });
+        }
+    });
+    app.post("/charge", async (req, res) => {
+        try {
+            const account = req.body.account ?? "account";
+            const result = await charge(account, req.body.charges ?? 10);
+            console.log(`Successfully charged account ${account}`);
+            res.status(200).json(result);
+        } catch (e) {
+            console.error("Error while charging account", e);
+            res.status(500).json({ error: String(e) });
+        }
+    });
+    return app;
+}
